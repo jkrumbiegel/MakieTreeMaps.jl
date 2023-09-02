be74a8863bc194543c88ecafbb4f679efb10bd47
Documenter.jl <documenter@juliadocs.github.io>
2023-09-02 13:27:25 +0000

    build based on 95213e3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MakieTreeMaps","category":"page"},{"location":"#MakieTreeMaps","page":"Home","title":"MakieTreeMaps","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MakieTreeMaps.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MakieTreeMaps]","category":"page"}]
+}
